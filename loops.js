c98737c624901959bc5e74fd473908bf142f2d1b
Oliver <oliver.grant@gmail.com>
2018-12-12 12:43:56 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,18 @@
+function forLoop(array) {
+  for (i=0;i<25;i++) {
+    if (i>1) {
+      array.push(`I am ${i} strange loops.`)
+    } else {
+      array.push("I am 1 strange loop.")
+    }
+  }
+  return array
+}
+
+function whileLoop(number) {
+  while (number>0) {
+    console.log(number)
+    number=number--
+  }
+  return console.log("done")
+}
