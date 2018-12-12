8835407587c3804f322483fbd7eba78e7c508869
Oliver <oliver.grant@gmail.com>
2018-12-12 12:58:54 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+function forLoop(array) {
+  for (i=0;i<25;i++) {
+    if (i>1) {
+      array.push(`I am ${i} strange loops.`)
+    } else {
+      array.push("I am 1 strange loop.")
+    }
+  }
+  return array
+}
+
+function whileLoop(number) {
+  while (number>0) {
+    console.log(number--)
+  }
+  return console.log("done")
+}
